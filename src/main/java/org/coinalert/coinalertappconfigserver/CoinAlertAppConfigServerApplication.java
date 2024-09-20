0e1ad982e6640c1f82fed128ec9c36ac6045310c
baekhun <baekhun2463@gmail.com>
2024-09-20 12:21:57 +0900

    config server

---
+++ src/main/java/org/coinalert/coinalertappconfigserver/CoinAlertAppConfigServerApplication.java
@@ -0,0 +1,13 @@
+package org.coinalert.coinalertappconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinAlertAppConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinAlertAppConfigServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/coinalert/coinalertappconfigserver/CoinAlertAppConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.coinalert.coinalertappconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinAlertAppConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
